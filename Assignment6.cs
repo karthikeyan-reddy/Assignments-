6f6d697bbac260511b01fdf4d5dab7d37bf09b7c
KARTHIKEYANREDDY MANDADI <109977197+karthikeyan-reddy@users.noreply.github.com>
2023-04-22 15:50:34 +0530

    Add files via upload

---
+++ Assignment6.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class Assignment6
+    {
+        ///Direct Method .........
+        static bool isValidDate(int year, int month, int day)
+        {
+            bool validate;
+            String givenDate= month.ToString() +"/"+ day.ToString()+"/"+year.ToString();
+            try
+            {
+                DateTime validdate = DateTime.Parse(givenDate);
+                validate = true;
+            }
+            catch (Exception)
+            {
+                validate = false;
+            }
+
+            return validate;
+        }
+        static void Main(string[] args)
+        {
+            getentries();
+        }
+
+        private static void getentries()
+        {
+            try
+            {
+                RETRY:
+                int year = UIConsole.GetNumber("Enter The Year in YYYY Format");
+                int month = UIConsole.GetNumber("Enter The Month in mm Format");
+                int day = UIConsole.GetNumber("Enter the day in dd Format");
+                //bool check = isValidDate(year, month, day);
+                bool check = Regularmethod(year, month, day);
+                if (check == true)
+                {
+                    UIConsole.PrintMessage("Valid Date Thanks!");
+                    Console.WriteLine("Press Any Key to Clear Screen");
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto RETRY;
+                }
+                else
+                {
+                    UIConsole.NegativeMessage("Invalid Date please check and enter again!");
+                    Console.WriteLine("Press Any Key to Clear Screen");
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto RETRY;
+                }
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Please Provide Numerical input only!!!!!!!");
+            }
+        }
+        ////Regular Method
+        static bool Regularmethod(int year, int month, int day)
+        {
+            bool validate =true;
+            if(month>12 || month<0)
+            {
+                validate = false;
+            }
+            else if (month == 02 || month == 2)
+            {
+                if (year%4==0)
+                {
+                    if (day>29 || day<0)
+                    {
+                        validate= false;
+                    }
+                }
+                else
+                {
+                    if (day>28 || day < 0)
+                    {
+                        validate = false;
+                    }
+                }
+            }
+            else if (month == 01 || month == 03 || month == 05 || month ==07 || month==08 || month==10|| month == 12 )
+            {
+                if(day>31 || day<0)
+                {
+                    validate = false;
+                }
+            }
+            else if (month == 04 || month == 06 || month == 09 || month == 11)
+            {
+                if (day > 30 || day < 0)
+                {
+                    validate = false;
+                }
+            }
+            else
+            {
+                validate = true;
+            }
+
+            return validate ;
+        }
+    }
+}
---
+++ Assignment7.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class Assignment7
+    {
+        static bool isPrimeNumber(int num)
+        {
+            if (num<=0)
+            {
+                return false;
+            }
+             else
+            {
+                if (num > 2)
+                {
+                    for (int i = 2; i < num / 2; i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            return false;
+                        }
+                    }
+                }
+               else if(num == 2 || num == 3)
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("1 is niether Prime nor Composite !! ");
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+        RETRY:
+            try
+            {
+                int number = UIConsole.GetNumber("Enter The Number to check whether it is Prime or not");
+                bool check = isPrimeNumber(number);
+                if(check==true)
+                {
+                    Console.WriteLine($"{number} is a Prime Number");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is Not a Prime Number");
+                }
+                goto RETRY;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Provide a Valid Number in Numerical Format");
+                goto RETRY;
+            }
+
+        }
+    }
+}
