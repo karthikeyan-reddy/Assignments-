bbff8062a4e8833939ce7ce859cd89d674f11e97
KARTHIKEYANREDDY MANDADI <109977197+karthikeyan-reddy@users.noreply.github.com>
2023-04-24 08:46:24 +0530

    Add files via upload

---
+++ Assignment11.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class Assignment11
+    {
+        static void Main(string[] args)
+        {
+            Requirements();
+        }
+
+        private static void Requirements()
+        {
+        RETRY:
+            Console.Clear();
+            try
+            {
+                double Principle = UIConsole.GetDouble("Enter the Principle Amount");
+                if(Principle<0)
+                {
+                    Console.WriteLine("Non Negative Numbers are Invalid!!!");
+                    Console.WriteLine("Press Any Key to Clear Screen.....");
+                    Console.ReadKey();
+                    goto RETRY;
+                }
+                double RateOfInterest = UIConsole.GetDouble("Enter the Rate Of Interest");
+                if (RateOfInterest < 0)
+                {
+                    Console.WriteLine("Non Negative Numbers are Invalid!!!");
+                    Console.WriteLine("Press Any Key to Clear Screen.....");
+                    Console.ReadKey();
+                    goto RETRY;
+                }
+                double term = UIConsole.GetDouble("Enter The term of Loan");
+                if (term < 0)
+                {
+                    Console.WriteLine("Non Negative Numbers are Invalid!!!");
+                    Console.WriteLine("Press Any Key to Clear Screen.....");
+                    Console.ReadKey();
+                    goto RETRY;
+                }
+                InterestCalculator(Principle, RateOfInterest, term);
+                Console.WriteLine("Press Any Key to Clear Screen.....");
+                Console.ReadKey();
+                goto RETRY;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Enter Valid Inputs!! inputs Should be Numeric and Non-negative.....");
+                goto RETRY;
+            }
+        }
+
+        private static void InterestCalculator(double principle, double rateOfInterest, double term)
+        {
+            double interest = (principle * term * rateOfInterest) / 100;
+            Console.WriteLine($"Interest Acquired For The {principle} with {rateOfInterest} Rate of interest for the Ter Period of {term} is {interest}");
+        }
+    }
+}
---
+++ Assignment9.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{ 
+    class Assignment9
+    {
+        public static string reverseByWords(string sentence)
+        {
+            string[] splitSentenceArray = sentence.Split(' ');
+            string ReverseSentense = "";
+            for (int i = splitSentenceArray.Length-1; i >= 0; i--)
+            {
+                if (i==0)
+                {
+                    ReverseSentense = ReverseSentense + splitSentenceArray[i];
+                }
+                else
+                {
+                    ReverseSentense = ReverseSentense + splitSentenceArray[i] + " ";
+                }
+            }
+            
+            return ReverseSentense;
+        }
+        static void Main(string[] args)
+        {
+            START:
+            string EnteredSentence = UIConsole.GetString("Enter the Senetence");
+            string result = reverseByWords(EnteredSentence);
+            UIConsole.ReadKey(result + "\nPress any key to clear Screen");
+            goto START;
+            
+        }
+    }
+}
